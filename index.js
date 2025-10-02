35d67f3aa5808b7a5c5cfb66a5d9590a2cf464f5
Abhishek0340 <abhishekshinde034@gmail.com>
2025-10-02 16:17:58 +0530

    second commit

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const Post = require('./models/Post');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  ['https://absbloger.netlify.app/', 'http://localhost:5173']
+));
+
 app.use(express.json());
 
 // Connect to MongoDB
